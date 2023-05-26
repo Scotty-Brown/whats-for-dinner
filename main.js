d760165a9bacce54118b829955700ac81fa7b850
Scotty Brown <scottybrown98@gmail.com>
2023-05-26 16:55:29 -0700

    Add query selectors

---
+++ main.js
@@ -0,0 +1,10 @@
+//pages
+var mainPage = document.querySelector('.main-content')
+//right/left box
+var leftBox = document.querySelector('.list')
+var rightBox = document.querySelector('.image')
+var cookPotImage = document.querySelector('cook-pot')
+//buttons
+var addRecipeB = document.querySelector('.ar')
+var letsCookB = document.querySelector('.lets-cook')
+
