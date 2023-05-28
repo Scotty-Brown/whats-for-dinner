6cf2ef5fe91e046bfa3232f1b20eac6d067b65d4
Scotty Brown <scottybrown98@gmail.com>
2023-05-28 16:03:04 -0700

    Fix minor styling issues

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ var cookPotImage = document.querySelector('.cook-pot')
 var rightBoxAfterClick = document.querySelector('.group-tag-right')
 var insertDishHere = document.querySelector('.desc-of-dish')
 var clearButton = document.querySelector('.clear-button')
+var noneSelectedMes = document.querySelector('.none-selected-message')
 //buttons
 var addRecipeB = document.querySelector('.ar')
 var letsCookB = document.querySelector('.lets-cook')
@@ -31,6 +32,15 @@ clearButton.addEventListener('click', function() {
 function showHide(show, hide) {
     show.classList.remove('hidden')
     hide.classList.add('hidden')
+    
+}
+
+function show(show) {
+    show.classList.remove('hidden')
+}
+
+function hide(hide) {
+    hide.classList.add('hidden')
 }
 
 function randomFood (array) {
@@ -46,15 +56,27 @@ function rbSelectedValue() {
 
     if(rb1.checked==true) {
         showHide(rightBoxAfterClick, cookPotImage)
+        hide(noneSelectedMes)
         insertDishHere.innerText = randomFood(Sides)
+        rb1.checked = false
     } else if(rb2.checked==true) {
         showHide(rightBoxAfterClick, cookPotImage)
+        hide(noneSelectedMes)
         insertDishHere.innerText = randomFood(Mains)
+        rb2.checked = false
     } else if(rb3.checked==true) {
         showHide(rightBoxAfterClick, cookPotImage)
+        hide(noneSelectedMes)
         insertDishHere.innerText = randomFood(Desserts)
+        rb3.checked = false
     } else if(rb4.checked==true) {
         showHide(rightBoxAfterClick, cookPotImage)
+        hide(noneSelectedMes)
         insertDishHere.innerText = `${randomFood(Mains)} with a side of ${randomFood(Sides)} and ${randomFood(Desserts)} for dessert!`
-    }  
+        rb4.checked = false
+    } else {
+        hide(cookPotImage)
+        hide(rightBoxAfterClick)
+        show(noneSelectedMes)
+    }
 }
