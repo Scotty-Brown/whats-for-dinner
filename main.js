f5d8986ffe30e6c3e326cbb398bddaddf535348c
Scotty Brown <scottybrown98@gmail.com>
2023-05-29 10:51:53 -0700

    Update naming convention for functions

--- main.js
+++ main.js
@@ -9,53 +9,52 @@ var rightBox = document.querySelector('.image')
 var cookPotImage = document.querySelector('.cook-pot')
 var rightBoxAfterClick = document.querySelector('.group-tag-right')
 var insertDishHere = document.querySelector('.desc-of-dish')
-var clearButton = document.querySelector('.clear-button')
 var noneSelectedMes = document.querySelector('.none-selected-message')
 var addedRecipeNotice = document.querySelector('.user-input-dish')
 var userInputAddPage = document.querySelector('.user-input-added')
-//add new recipe
+//add new recipe form
 var addRecipeForm = document.querySelector('.add-recipe-display')
 //buttons
 var addRecipeB = document.querySelector('.add-recipe-b')
 var letsCookB = document.querySelector('.lets-cook')
+var clearButton = document.querySelector('.clear-button')
 var addNew = document.querySelector('.add')
 
 // EVENT LISTENERS
 
 letsCookButton.addEventListener('click', function(event) {
     event.preventDefault()
-    showHide(rightBoxAfterClick, cookPotImage)
-    rbSelectedValue()
+    showHideContent(rightBoxAfterClick, cookPotImage)
+    generateRandomDish()
 })
 
 clearButton.addEventListener('click', function(event) {
     event.preventDefault()
-    showHide(cookPotImage, rightBoxAfterClick)
+    showHideContent(cookPotImage, rightBoxAfterClick)
 })
 
 addRecipeB.addEventListener('click', function() {
-    // location.reload(addRecipeForm)
-    show(addRecipeForm)
+    showContent(addRecipeForm)
 })
 
 addNew.addEventListener('click', function(event) {
     event.preventDefault
-    dropDownSelection(event)
+    addARecipe(event)
 })
 
 // FUNKY FUNCTIONS
 
-function showHide(show, hide) {
+function showHideContent(show, hide) {
     show.classList.remove('hidden')
     hide.classList.add('hidden')
     
 }
 
-function show(show) {
+function showContent(show) {
     show.classList.remove('hidden')
 }
 
-function hide(hide) {
+function hideContent(hide) {
     hide.classList.add('hidden')
 }
 
@@ -68,45 +67,45 @@ function clearForm() {
 
 }
 
-function rbSelectedValue() {
+function generateRandomDish() {
     var rb1 = document.getElementById("rb1")
     var rb2 = document.getElementById("rb2")
     var rb3 = document.getElementById("rb3")
     var rb4 = document.getElementById("rb4")
 
     if(rb1.checked==true) {
-        showHide(rightBoxAfterClick, cookPotImage)
-        hide(noneSelectedMes)
-        hide(userInputAddPage)
+        showHideContent(rightBoxAfterClick, cookPotImage)
+        hideContent(noneSelectedMes)
+        hideContent(userInputAddPage)
         insertDishHere.innerText = randomFood(Sides)
         rb1.checked = false
     } else if(rb2.checked==true) {
-        showHide(rightBoxAfterClick, cookPotImage)
-        hide(noneSelectedMes)
-        hide(userInputAddPage)
+        showHideContent(rightBoxAfterClick, cookPotImage)
+        hideContent(noneSelectedMes)
+        hideContent(userInputAddPage)
         insertDishHere.innerText = randomFood(Mains)
         rb2.checked = false
     } else if(rb3.checked==true) {
-        showHide(rightBoxAfterClick, cookPotImage)
-        hide(noneSelectedMes)
-        hide(userInputAddPage)
+        showHideContent(rightBoxAfterClick, cookPotImage)
+        hideContent(noneSelectedMes)
+        hideContent(userInputAddPage)
         insertDishHere.innerText = randomFood(Desserts)
         rb3.checked = false
     } else if(rb4.checked==true) {
-        showHide(rightBoxAfterClick, cookPotImage)
-        hide(noneSelectedMes)
-        hide(userInputAddPage)
+        showHideContent(rightBoxAfterClick, cookPotImage)
+        hideContent(noneSelectedMes)
+        hideContent(userInputAddPage)
         insertDishHere.innerText = `${randomFood(Mains)} with a side of ${randomFood(Sides)} and ${randomFood(Desserts)} for dessert!`
         rb4.checked = false
     } else {
-        hide(cookPotImage)
-        hide(rightBoxAfterClick)
-        hide(userInputAddPage)
-        show(noneSelectedMes)
+        hideContent(cookPotImage)
+        hideContent(rightBoxAfterClick)
+        hideContent(userInputAddPage)
+        showContent(noneSelectedMes)
     }
 }
 
-function dropDownSelection (event) {
+function addARecipe(event) {
     var valueInput = document.getElementById("food-type-dd").value
     var userInput = document.getElementById('ar-actual').value
     
@@ -117,29 +116,29 @@ function dropDownSelection (event) {
         event.preventDefault()
         Sides.push(userInput)
         addedRecipeNotice.innerText = `${userInput} has been added to Side Dishes!`
-        show(userInputAddPage)
-        hide(cookPotImage)
-        hide(rightBoxAfterClick)
-        hide(addRecipeForm)
-        hide(noneSelectedMes)
+        showContent(userInputAddPage)
+        hideContent(cookPotImage)
+        hideContent(rightBoxAfterClick)
+        hideContent(addRecipeForm)
+        hideContent(noneSelectedMes)
     } else if (valueInput === "main") {
         event.preventDefault()
         Mains.push(userInput)
         addedRecipeNotice.innerText = `${userInput} has been added to Main Dishes!`
-        show(userInputAddPage)
-        hide(cookPotImage)
-        hide(rightBoxAfterClick)
-        hide(addRecipeForm)
-        hide(noneSelectedMes)
+        showContent(userInputAddPage)
+        hideContent(cookPotImage)
+        hideContent(rightBoxAfterClick)
+        hideContent(addRecipeForm)
+        hideContent(noneSelectedMes)
     } else if (valueInput === "dessert") {
         event.preventDefault()
         Desserts.push(userInput)
         addedRecipeNotice.innerText = `${userInput} has been added to Dessert Dishes!`
-        show(userInputAddPage)
-        hide(cookPotImage)
-        hide(rightBoxAfterClick)
-        hide(addRecipeForm)
-        hide(noneSelectedMes)
+        showContent(userInputAddPage)
+        hideContent(cookPotImage)
+        hideContent(rightBoxAfterClick)
+        hideContent(addRecipeForm)
+        hideContent(noneSelectedMes)
     }
 
 }
